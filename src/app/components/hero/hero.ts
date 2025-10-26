29a463dde5fc17e53291cb1ff7401591a84014b2
zotel1 <zotelsigel@gmail.com>
2025-10-26 05:12:10 -0300

    cambiamos imagenes del hero deacuerdo al tamaño de las pantallas

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,49 +1,48 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrls: ['./hero.css']
 })
 export class Hero implements OnInit, OnDestroy {
+  imagenActual = '';
+  indice = 0;
+  intervalo: any;
 
-  images: string[] = [
-    'assets/flynn/1.jpeg',
-    'assets/flynn/2.jpeg',
-    'assets/flynn/3.jpeg',
+  imagenesDesktop = [
     'assets/flynn/4.jpeg',
-    'assets/flynn/5.jpeg',
     'assets/flynn/6.jpeg',
     'assets/flynn/7.jpeg',
-    'assets/flynn/8.jpeg',
     'assets/flynn/9.jpeg',
     'assets/flynn/10.jpeg'
   ];
 
-  currentImage: string = this.images[0];
-  private intervalId: any;
+  imagenesMobile = [
+    'assets/flynn/1.jpeg',
+    'assets/flynn/2.jpeg',
+    'assets/flynn/3.jpeg',
+    'assets/flynn/5.jpeg',
+    'assets/flynn/8.jpeg'
+  ];
 
-  ngOnInit(): void {
-    this.startImageRotation();
+  get imagenes() {
+    return window.innerWidth > 768 ? this.imagenesDesktop : this.imagenesMobile;
   }
 
-  ngOnDestroy(): void {
-    if (this.intervalId) clearInterval(this.intervalId);
+  ngOnInit() {
+    this.cambiarImagen(); // inicializa la primera imagen
+    this.intervalo = setInterval(() => this.cambiarImagen(), 6000);
   }
 
-  startImageRotation(): void {
-    this.intervalId = setInterval(() => {
-      const next = this.getRandomImage(this.currentImage);
-      this.currentImage = next;
-    }, 3000);
+  cambiarImagen() {
+    const lista = this.imagenes;
+    this.imagenActual = lista[this.indice];
+    this.indice = (this.indice + 1) % lista.length;
   }
 
-  getRandomImage(current: string): string {
-    const otherImages = this.images.filter(img => img !== current);
-    const randomIndex = Math.floor(Math.random() * otherImages.length);
-    return otherImages[randomIndex];
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
   }
 }
