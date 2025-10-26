a03717d9457c732dc5920e6ecd3b026e0fc99799
zotel1 <zotelsigel@gmail.com>
2025-10-26 01:52:47 -0300

    editamos la ruta de app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Reservas } from './pages/reservas/reservas';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: Reservas },
+    { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
