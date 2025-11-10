865656602e1637ba263f42549be39ad27a4e8c10
zotel1 <zotelsigel@gmail.com>
2025-11-10 07:20:25 -0300

    agregamos la ruta de dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Reservas } from './pages/reservas/reservas';
 import { Chatbot } from './components/chatbot/chatbot';
+import { ReservasDashboard } from './components/reservas-dashboard/reservas-dashboard';
 
 export const routes: Routes = [
   { path: '', component: Chatbot },
   { path: 'reservas', component: Reservas },
-  { path: '**', redirectTo: '' },
-  
+  {  path: 'admin/reservas', component: ReservasDashboard  },
+  { path: '**', redirectTo: '' }
 ];
