84a19d1d375220d9c837f16795db5a44e2f225e7
zotel1 <zotelsigel@gmail.com>
2025-12-28 10:59:45 -0300

    update, politica-privacidad.ts

--- src/app/pages/politica-privacidad/politica-privacidad.ts
+++ src/app/pages/politica-privacidad/politica-privacidad.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-politica-privacidad',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './politica-privacidad.html',
   styleUrl: './politica-privacidad.css',
 })
 export class PoliticaPrivacidad {
+    readonly currentYear: number = new Date().getFullYear();
 
 }
