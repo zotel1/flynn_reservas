88862afa06cf268e9aef207b449d95821646b269
zotel1 <zotelsigel@gmail.com>
2025-12-28 11:00:34 -0300

    update , terminoServicios

--- src/app/pages/terminos-servicio/terminos-servicio.ts
+++ src/app/pages/terminos-servicio/terminos-servicio.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-terminos-servicio',
   imports: [],
+  standalone: true,
   templateUrl: './terminos-servicio.html',
   styleUrl: './terminos-servicio.css',
 })
 export class TerminosServicio {
-
+readonly currentYear: number = new Date().getFullYear();
 }
