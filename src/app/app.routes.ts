bf8fa98ddcc5282b2321684f7891e1e53c6c9421
zotel1 <zotelsigel@gmail.com>
2025-12-28 17:51:48 -0300

    update component app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { Reservas } from './pages/reservas/reservas';
 import { Chatbot } from './components/chatbot/chatbot';
 import { ReservasDashboard } from './components/reservas-dashboard/reservas-dashboard';
-import { PoliticaPrivacidadComponent } from './pages/politica-privacidad/politica-privacidad';
-import { TerminosServicioComponent } from './pages/terminos-servicio/terminos-servicio';
+import { PoliticaPrivacidad } from './pages/politica-privacidad/politica-privacidad';
+import { TerminosServicio } from './pages/terminos-servicio/terminos-servicio';
 
 export const routes: Routes = [
   { path: '', component: Chatbot },
   { path: 'reservas', component: Reservas },
   {  path: 'admin/reservas', component: ReservasDashboard  },
-  { path: 'politica-privacidad', component: PoliticaPrivacidadComponent },
-  { path: 'terminos-servicio', component: TerminosServicioComponent },
+  { path: 'politica-privacidad', component: PoliticaPrivacidad },
+  { path: 'terminos-servicio', component: TerminosServicio },
   { path: '**', redirectTo: '' }
 ];
