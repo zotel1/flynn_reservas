e427f269ed92771fb9469872606fcbcd60361536
zotel1 <zotelsigel@gmail.com>
2026-01-03 20:01:13 -0300

    adaptamos nuestro jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,13 @@
 import { createCjsPreset } from 'jest-preset-angular/presets';
 
 export default {
-    ...createCjsPreset(),
+    preset: 'jest-preset-angular/presets/defaults-esm',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     testEnvironment: 'jsdom',
     testMatch: ['<rootDir>/**/?(*.)+(spec).ts'],
-    testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/out-tsc/', '<rootDir>/src/test.ts'],
+    testPathIgnorePatterns: [
+        '<rootDir>/dist/',
+        '<rootDir>/out-tsc/',
+        '<rootDir>/src/test.ts',
+    ],
 };
