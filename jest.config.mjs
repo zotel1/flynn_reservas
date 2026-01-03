00a5534fbc3fa49e284d59626b0e43e0258a4bb0
zotel1 <zotelsigel@gmail.com>
2026-01-03 20:15:26 -0300

    adaptamos nuevamente el jest.config.mjs y encontramos 12 errores

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,13 +1,7 @@
-import { createCjsPreset } from 'jest-preset-angular/presets';
-
 export default {
-    preset: 'jest-preset-angular/presets/defaults-esm',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    testEnvironment: 'jsdom',
-    testMatch: ['<rootDir>/**/?(*.)+(spec).ts'],
-    testPathIgnorePatterns: [
-        '<rootDir>/dist/',
-        '<rootDir>/out-tsc/',
-        '<rootDir>/src/test.ts',
-    ],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/**/*.spec.ts'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/out-tsc/', '<rootDir>/src/test.ts'],
 };
